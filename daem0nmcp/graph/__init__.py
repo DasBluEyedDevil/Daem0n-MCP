bf22a36b39cb8271ff271f48cf3323eba722fa1f
Devil <dasblueeyeddevil@gmail.com>
2026-01-22 22:54:35 -0500

    feat(01-02): export EntityResolver from graph module
    - Add EntityResolver to graph module exports
    - Enables: from daem0nmcp.graph import EntityResolver

--- daem0nmcp/graph/__init__.py
+++ daem0nmcp/graph/__init__.py
@@ -1,4 +1,5 @@
 """Graph module for GraphRAG knowledge graph operations."""
 from .knowledge_graph import KnowledgeGraph
+from .entity_resolver import EntityResolver
 
-__all__ = ["KnowledgeGraph"]
+__all__ = ["KnowledgeGraph", "EntityResolver"]
