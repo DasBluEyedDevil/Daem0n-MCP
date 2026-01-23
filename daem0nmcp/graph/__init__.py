d6bfef91ffceee7fb5751ca1f9f624c6457c5998
Devil <dasblueeyeddevil@gmail.com>
2026-01-22 23:14:15 -0500

    feat(01-03): export Leiden functions from graph module
    - Add run_leiden_on_networkx, LeidenConfig, get_community_stats to exports
    - Reorder imports alphabetically
    - Update __all__ list with all public APIs

--- daem0nmcp/graph/__init__.py
+++ daem0nmcp/graph/__init__.py
@@ -1,5 +1,22 @@
 """Graph module for GraphRAG knowledge graph operations."""
-from .knowledge_graph import KnowledgeGraph
 from .entity_resolver import EntityResolver
+from .knowledge_graph import KnowledgeGraph
+from .leiden import LeidenConfig, get_community_stats, run_leiden_on_networkx
+from .traversal import (
+    find_related_memories,
+    get_graph_metrics,
+    trace_causal_chain,
+    trace_knowledge_evolution,
+)
 
-__all__ = ["KnowledgeGraph", "EntityResolver"]
+__all__ = [
+    "KnowledgeGraph",
+    "EntityResolver",
+    "run_leiden_on_networkx",
+    "LeidenConfig",
+    "get_community_stats",
+    "trace_causal_chain",
+    "find_related_memories",
+    "trace_knowledge_evolution",
+    "get_graph_metrics",
+]
